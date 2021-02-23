e32b7d2a4185d4ed201b1e83bb799ec18e8fee13
Heigar Javier Pichardo <heigar_p@hotmail.es>
2021-02-22 21:59:39 -0400

    a

--- app/Brands.php
+++ app/Brands.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brands extends Model
 {
+    protected $table = "Brands";
     protected $primaryKey = "idbrand";
     protected $fillable = ["brand"];
 
